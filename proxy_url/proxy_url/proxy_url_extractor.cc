e847e278a137bed8d471b874af717fd106f4bd0d
ginues109 <ginues109@qq.com>
2014-08-04 21:32:24 +0800

    Extractor completed
    six lines were added.

--- proxy_url/proxy_url/proxy_url_extractor.cc
+++ proxy_url/proxy_url/proxy_url_extractor.cc
@@ -100,6 +100,42 @@ namespace qh
     {
 #if 1
         //TODO 请面试者在这里添加自己的代码实现以完成所需功能
+		Tokener token(raw_url);
+        token.skipTo('?');
+        token.next(); //skip one char : '?' 
+        std::string key;
+        while (!token.isEnd()) {
+            key = token.nextString('=');
+			if(key.rfind('&')!=-1){
+				token.back(key.size());
+			}
+			if(sub_url.rfind('&')!=-1){
+				sub_url="";
+			}
+            if (keys.find(key) != keys.end()) {
+                const char* curpos = token.getCurReadPos();
+                int nreadable = token.getReadableSize();
+
+                /**
+                * case 1: 
+                *  raw_url="http://www.microsofttranslator.com/bv.aspx?from=&to=zh-chs&a=http://hnujug.com/&xx=yy"
+                *  sub_url="http://hnujug.com/"
+                */
+                sub_url = token.nextString('&');
+
+                if (sub_url.empty() && nreadable > 0) {
+                    /**
+                    * case 2: 
+                    * raw_url="http://www.microsofttranslator.com/bv.aspx?from=&to=zh-chs&a=http://hnujug.com/"
+                    * sub_url="http://hnujug.com/"
+                    */
+                    assert(curpos);
+                    sub_url.assign(curpos, nreadable);
+                }
+            }
+            token.skipTo('&');
+            token.next();//skip one char : '&'
+        }
 #else
         //这是一份参考实现，但在特殊情况下工作不能符合预期
         Tokener token(raw_url);
